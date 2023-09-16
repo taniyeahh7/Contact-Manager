6ef17c291fb6779c7ed0a842417263a925147033
taniyeahh7 <taniya.ahmed734@gmail.com>
2023-09-16 12:31:14 +0530

    prelim additions to contactroutes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -4,7 +4,26 @@ const router=express.Router();
 
 
 
-router.route("/",(req,res)=>{//basically want to send a
+router.route("/").get((req,res)=>{//basically want to send a
     res.status(200).json({message:"Get all contacts"});
 });
 
+router.route("/").post((req,res)=>{//putting a request probably new
+    res.status(200).json({message:"Post all contacts"});
+});
+
+router.route("/:id").get((req,res)=>{//getting for the particular 
+    res.status(200).json({message:`Get contact for ${req.params.id}`});
+});
+
+
+router.route("/:id").put((req,res)=>{//put for is for updating
+    res.status(200).json({message:`Update contact for ${req.params.id}`});
+});
+
+router.route("/:id").delete((req,res)=>{
+    res.status(200).json({message:`Delete contact for ${req.params.id}`});
+});
+
+
+module.exports=router;//basically mapping a router and all logiv requred to map
